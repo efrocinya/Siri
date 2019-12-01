0e3e55cae60db2f7e9c08527994b45d014c5082d
efrocinya <soniashes@mail.ru>
2019-12-01 13:49:55 +0300

    first commit

---
+++ City which not exist/Assets/Scripts/ManagerMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ManagerMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void onCardsFromMenu()
+    {
+        SceneManager.LoadScene("CardsScene");
+    }
+    public void onARCameraFromMenu()
+    {
+        SceneManager.LoadScene("ARCamera");
+    }
+    public void onShowSendFile()
+    {
+        SceneManager.LoadScene("SendFile");
+    }
+    public void onShowMenuFromShare()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+}
